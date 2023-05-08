852cf4f39706036ef41f509efd796edceb64d125
Kyle Dixler <kyle@pulumi.com>
2023-05-08 14:42:59 -0700

    added a resource that function serializes

--- packages/cloud/index.ts
+++ packages/cloud/index.ts
@@ -1,4 +1,12 @@
 import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
 import * as core from 'example-core'
 
 // serverless pulumi script goes here that uses core.f
+new aws.lambda.CallbackFunction('my-function', {
+    callback: async (event, context) => {
+        console.log(core.f());
+        return;
+    },
+});
+export const f = core.f()
